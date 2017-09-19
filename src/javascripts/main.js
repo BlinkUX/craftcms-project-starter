b91430d9f44f5d017337934dd3c20f6f48abcfc5
Philipp Rappold <philipp@rppld.co>
2017-09-19 22:37:41 +0200

    Change the way fonts are included

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,8 +3,6 @@ import { HTTP } from "./http-common"
 import s from "./store"
 import Logo from "./components/Logo"
 import "../stylesheets/style.scss"
-import "../assets/ArchivoBlack-Regular.woff2"
-import "../assets/ArchivoBlack-Regular.woff"
 
 console.log("Global.js file has imported")
 
