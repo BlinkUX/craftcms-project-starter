e64c94f243b99c54831fc1b4e4968468bc702dfb
Philipp Rappold <philipp@rppld.co>
2017-10-02 21:30:32 +0200

    Edit serialize function and add button reset

--- src/javascripts/helpers.js
+++ src/javascripts/helpers.js
@@ -47,71 +47,78 @@ const helpers = {
     }
   },
 
-  /* eslint-disable */
-  serialize(form) {
+  serialize (form) {
     if (!form || form.nodeName !== "FORM") {
-      return;
+      return
     }
-    var i, j, q = [];
+
+    let i = []
+    let j = []
+    let q = []
+
     for (i = form.elements.length - 1; i >= 0; i = i - 1) {
       if (form.elements[i].name === "") {
-        continue;
+        continue
       }
+
       switch (form.elements[i].nodeName) {
-        case 'INPUT':
+        case "INPUT":
           switch (form.elements[i].type) {
-            case 'text':
-            case 'number':
-            case 'email':
-            case 'hidden':
-            case 'password':
-            case 'button':
-            case 'reset':
-            case 'submit':
-              q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-              break;
-            case 'checkbox':
-            case 'radio':
+            case "text":
+            case "number":
+            case "email":
+            case "tel":
+            case "hidden":
+            case "password":
+            case "button":
+            case "reset":
+            case "submit":
+              q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value))
+              break
+            case "checkbox":
+            case "radio":
               if (form.elements[i].checked) {
-                q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+                q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value))
               }
-              break;
-            case 'file':
-              break;
+              break
+            case "file":
+              break
           }
-          break;
-        case 'TEXTAREA':
-          q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-          break;
-        case 'SELECT':
+          break
+
+        case "TEXTAREA":
+          q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value))
+          break
+
+        case "SELECT":
           switch (form.elements[i].type) {
-            case 'select-one':
-              q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-              break;
-            case 'select-multiple':
+            case "select-one":
+              q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value))
+              break
+
+            case "select-multiple":
               for (j = form.elements[i].options.length - 1; j >= 0; j = j - 1) {
                 if (form.elements[i].options[j].selected) {
-                  q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].options[j].value));
+                  q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].options[j].value))
                 }
               }
-              break;
+              break
           }
-          break;
-        case 'BUTTON':
+          break
+
+        case "BUTTON":
           switch (form.elements[i].type) {
-            case 'reset':
-            case 'submit':
-            case 'button':
-              q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-              break;
+            case "reset":
+            case "submit":
+            case "button":
+              q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value))
+              break
           }
-          break;
+          break
       }
     }
-    return q.join("&");
+    return q.join("&")
   },
-  /* eslint-enable */
-
 }
 
 export default helpers
