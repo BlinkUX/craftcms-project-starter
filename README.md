3c6562c869e6b1126e3866ed4c48d2f2455b7f13
Philipp Rappold <philipp@rppld.co>
2017-12-20 15:03:58 +0100

    Update to Craft 3 RC3 and remove Patrol plugin

--- README.md
+++ README.md
@@ -69,7 +69,6 @@ This project scaffolding is rather opinionated and makes use of the following to
 
 * [Craft CMS](https://craftcms.com/) - Craft is a content-first CMS that aims to make life enjoyable for developers and content managers alike.
 * [Element API for Craft CMS](https://github.com/craftcms/element-api) - Craft CMS plugin to create a JSON API/Feed for your elements in Craft.
-* [Patrol for Craft CMS](https://github.com/selvinortiz/craft.patrol) - Patrol simplifies SSL and maintenance routing for sites built with Craft.
 * [Webpack](https://webpack.js.org/) - A bundler for JavaScript and friends with [code splitting](https://webpack.js.org/guides/code-splitting/) and [Hot Module Replacement](https://webpack.js.org/guides/hot-module-replacement/) set up.
 * [Sass](http://sass-lang.com/) - CSS extension language
 * [Vue.js](https://vuejs.org/) - The Progressive JavaScript Framework
--- config/general.php
+++ config/general.php
@@ -1,25 +1,58 @@
 <?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ */
 
 return [
-  '*' => [
-    'defaultWeekStartDay' => 1,
-    'enableCsrfProtection' => true,
-    'omitScriptNameInUrls' => true,
-    'preserveImageColorProfiles' => true,
-    'defaultSearchTermOptions' => [
-      // Enables fuzzy search
-      'subLeft' => true,
-      'subRight' => true,
+    // Global settings
+    '*' => [
+        // Default Week Start Day (0 = Sunday, 1 = Monday...)
+        'defaultWeekStartDay' => 1,
+
+        // Enable CSRF Protection (recommended, will be enabled by default in Craft 3)
+        'enableCsrfProtection' => true,
+
+        // Whether "index.php" should be visible in URLs
+        'omitScriptNameInUrls' => true,
+
+        // Control Panel trigger word
+        'cpTrigger' => 'admin',
+
+        // The secure key Craft will use for hashing and encrypting data
+        'securityKey' => getenv('SECURITY_KEY'),
+
+        // Enables fuzzy search
+        'defaultSearchTermOptions' => [
+          'subLeft' => true,
+          'subRight' => true,
+        ],
+
+        'preserveImageColorProfiles' => true,
+        'imageDriver' => 'imagick',
+        'generateTransformsBeforePageLoad' => true,
     ],
-    'imageDriver' => 'imagick',
-    'generateTransformsBeforePageLoad' => true,
-  ],
 
-  'dev' => [
-    'devMode' => true,
-  ],
+    // Dev environment settings
+    'dev' => [
+        // Base site URL
+        'siteUrl' => null,
 
-  'frb' => [
-    'cooldownDuration' => 0,
-  ],
+        // Dev Mode (see https://craftcms.com/support/dev-mode)
+        'devMode' => true,
+    ],
+
+    // Staging environment settings
+    'staging' => [
+        // Base site URL
+        'siteUrl' => null,
+    ],
+
+    // Production environment settings
+    'production' => [
+        // Base site URL
+        'siteUrl' => null,
+    ],
 ];
